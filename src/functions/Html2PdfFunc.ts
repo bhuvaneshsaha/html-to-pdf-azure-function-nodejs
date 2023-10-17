02c048748b8992a0c55237bd5980742e7b082df4
Bhuvanesh <bhuvaneshmib@gmail.com>
2023-10-17 13:39:41 +0530

    first commit

---
+++ src/functions/Html2PdfFunc.ts
@@ -0,0 +1,39 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { chromium } from "playwright-chromium";
+
+export async function Html2PdfFunc(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+
+  try {
+
+    const htmlContentFromBody = await request.text();
+
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+
+    await page.setContent(htmlContentFromBody);
+
+    const pdfBuffer = await page.pdf({ format: "a4" });
+    // const screenshotBuffer = 
+    //     await page.screenshot({ fullPage: true });
+    await browser.close();
+
+    return {
+      body: pdfBuffer,
+      headers: {
+        "content-type": "application/pdf",
+      }
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      body: error.message,
+    };
+  }
+
+};
+
+app.http('Html2PdfFunc', {
+  methods: ['GET', 'POST'],
+  authLevel: 'anonymous',
+  handler: Html2PdfFunc
+});
